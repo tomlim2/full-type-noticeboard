a6aad8a1531d35a2053258832fbbaa85cee74395
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-15 00:19:39 +0900

    Added: create post function

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,20 +1,22 @@
 import express, { Request, Response, Application } from "express";
 const app: Application = express();
 
-const posts: { id: number; title: string; desc: string }[] = [
-  { id: 0, title: "Available", desc: "dummy 1" },
-  { id: 1, title: "Ready", desc: "dummy 2" },
-  { id: 2, title: "Started", desc: "dummy 3" },
+const posts: { id: string; title: string; desc: string }[] = [
+  { id: "0", title: "Available", desc: "dummy 1" },
+  { id: "1", title: "Ready", desc: "dummy 2" },
+  { id: "2", title: "Started", desc: "dummy 3" },
 ];
 
+app.use(express.urlencoded({ extended: false }));
+
 app.post("/api/post", (req: Request, res: Response): void => {
+  console.log(req);
   posts.push(req.body);
-  console.log(req.body)
-  res.send(req.body);
+  res.status(200).json(req.body);
 });
 
 app.get("/api/post", (req: Request, res: Response): void => {
-  res.send(posts);
+  res.status(200).send(posts);
 });
 
 const port = 5000;
