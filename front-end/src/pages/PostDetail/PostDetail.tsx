50e7b640744ef604c5885bc6e65386c9ccd0fea9
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-17 22:03:20 +0900

    Added: webpack setting to set publicPath to figure route refreshing problem

--- front-end/src/pages/PostDetail/PostDetail.tsx
+++ front-end/src/pages/PostDetail/PostDetail.tsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Post from "../../models/posts";
 import "./PostDetail.css";
 
 const PostDetail = () => {
-//   const [post, setPost] = useState({});
-
-//   useEffect(() => {
-//     fetch("http://localhost:5000/api/post/123")
-//       .then((res) => res.json())
-//       .then((data) => setPost(data));
-//   }, []);
-//   console.log(post)
+  const [post, setPost] = useState<Post>();
+  const location = useLocation();
+  const path =
+    location.pathname.split("/")[location.pathname.split("/").length - 1];
+    console.log(location)
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/post/${path}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, []);
+  console.log(post);
   return (
     <div className="post-detail">
-      글번호:123 / 제목:hihi / 내용:why / 작성자:meme / 작성일:createdAt
+      글번호:{post&&post.postNumber} / 제목:{post&&post.title} / 내용:{post&&post.content} /
+      작성자:{post&&post.writer} / 작성일:{post&&post.editedAt}
     </div>
   );
 };
